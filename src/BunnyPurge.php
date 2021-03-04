4a4eec9e23a419ef1f16704918befca393d1185c
Lars Pinkenburg <lars.29palms@gmail.com>
2021-03-04 14:53:16 +0100

    initial commit

---
+++ src/BunnyPurge.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Incapption\BunnyPurge;
+
+/**
+ * Bunny CDN class for purging files
+ * @version  1.0
+ * @author incapption
+ */
+class BunnyPurge
+{
+	public function __construct()
+	{
+		
+	}
+}
